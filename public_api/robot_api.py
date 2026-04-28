bf90f55f389ad922afd198c101270968c5c9ef5b
Azat Kashapov <a.kashapov@rkeeper.ru>
2026-04-29 02:55:07 +0300

    robot_api

---
+++ public_api/robot_api.py
@@ -0,0 +1,22 @@
+import pure_robot as robot
+
+class RobotAPI:
+    def __init__(self):
+        self._state = robot.RobotState(x=0, y=0, angle=0, state=robot.WATER)
+        self._transfer = robot.transfer_to_cleaner
+
+    def move(self, distance: int):
+        self._state = robot.move(self._transfer, distance, self._state)
+
+    def turn(self, angle: int):
+        self._state = robot.turn(self._transfer, angle, self._state)
+
+    def set_mode(self, mode: str):
+        """Режим: 'water', 'soap', 'brush'"""
+        self._state = robot.set_state(self._transfer, mode, self._state)
+
+    def start(self):
+        self._state = robot.start(self._transfer, self._state)
+
+    def stop(self):
+        self._state = robot.stop(self._transfer, self._state)
